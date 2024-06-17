2ff03e2824268a9ca01b9fa9979f59e0bd46d614
Koravel <koravelalex@gmail.com>
2024-06-18 00:41:43 +0300

    Main functionality

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace PracticeFlowControl
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
